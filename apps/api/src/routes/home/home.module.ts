d1c250ef38354a9e45507dceb54009731d78cc60
Josue Monteiro <jusi.monteiro@protonmail.com>
2024-05-14 21:16:44 +0100

    Added @nest/config

--- apps/api/src/routes/home/home.module.ts
+++ apps/api/src/routes/home/home.module.ts
@@ -3,9 +3,10 @@ import { HomeController } from './home.controller';
 import { HomeService } from './home.service';
 import { AuthModule } from '../auth/auth.module';
 import { UsersModule } from '../users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule, ConfigModule.forRoot()],
   controllers: [HomeController],
   providers: [HomeService],
 })

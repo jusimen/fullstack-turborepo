55f0dea6d0e0aea0278ab0f076604c4477579084
Josue Monteiro <jusi.monteiro@protonmail.com>
2024-05-14 21:17:05 +0100

    Added Config Module

--- apps/api/src/routes/auth/auth.module.ts
+++ apps/api/src/routes/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaModule } from 'src/services/prisma/prisma.module';
 import { UtilsModule } from 'src/services/utils/utils.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UtilsModule, PrismaModule],
+  imports: [UtilsModule, PrismaModule, ConfigModule],
   controllers: [AuthController],
   providers: [AuthService],
   exports: [AuthService],
